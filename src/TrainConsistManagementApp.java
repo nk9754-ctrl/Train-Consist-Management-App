698d16e99d7c4c21208bcee91549c7fca00691c5
nk9754-ctrl <nikhilsaikandru6@gmail.com>
2026-03-24 09:49:32 +0530

    UC2:Track Unique BVogie IDs

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,35 +1,26 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+
+import java.util.*;
 
 
 public class TrainConsistManagementApp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("==================================");
-        System.out.println("UC2-Add Passenger Bogies to Train");
+        System.out.println("UC2- Track Unique Bogie IDs");
         System.out.println("===============================\n");
-        List<String> passengerBogies = new ArrayList<>();
-        passengerBogies.add("Sleeper");
-        passengerBogies.add("Ac Chair");
-        passengerBogies.add("First Class");
 
-        System.out.println("Ater Adding Bogies :");
-        System.out.println("Passenger Boggies: " + passengerBogies);
+        HashSet<String> bogies = new HashSet<>();
+        bogies.add("BG101");
+        bogies.add("BG102");
+        bogies.add("BG103");
+        bogies.add("BG104");
+        bogies.add("BG101");
+        bogies.add("BG102");
 
-        passengerBogies.remove("AC Chair");
-        System.out.println("Ater Removing Bogie");
-        System.out.println("Passenger Boggies: " + passengerBogies);
+        System.out.println("Bogies IDs After Insertion:");
+        System.out.println("Passenger Boggies: " + bogies);
 
-        passengerBogies.add("Second Class");
-        System.out.println("Ater Adding Bogies :");
-        System.out.println("Passenger Boggies: " + passengerBogies);
 
-        System.out.println("Checking if Sleeper exis");
-        boolean isPresent = passengerBogies.contains("Sleeper");
-
-        System.out.println("Final Train Passenger Consist: ");
-        System.out.println("Passenger Boggies: " + passengerBogies);
     }
 }
 
