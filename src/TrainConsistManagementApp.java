20a907d5ed96c0461a8d6ebca33c5ec4aabb567d
nk9754-ctrl <nikhilsaikandru6@gmail.com>
2026-04-09 10:54:24 +0530

    UC10:Count Total Seats in Train (reduce)

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,55 +1,19 @@
-import java.util.*;
-import java.util.stream.Collectors;
-
-// Passenger Bogie class
-class PassengerBogie {
-    private String type;
-    private int capacity;
-
-    public PassengerBogie(String type, int capacity) {
-        this.type = type;
-        this.capacity = capacity;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public int getCapacity() {
-        return capacity;
-    }
-
-    @Override
-    public String toString() {
-        return "PassengerBogie{" +
-                "type='" + type + '\'' +
-                ", capacity=" + capacity +
-                '}';
-    }
-}
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class TrainConsistManagementApp {
     public static void main(String[] args) {
-        // Original bogie list (reused from UC7)
-        List<PassengerBogie> bogies = Arrays.asList(
-                new PassengerBogie("Sleeper", 50),
-                new PassengerBogie("AC Chair", 80),
-                new PassengerBogie("First Class", 100),
-                new PassengerBogie("Economy", 60)
-        );
+        Scanner sc =new Scanner(System.in);
+        System.out.println("==================================");
+        System.out.println("===Train Consist Management App===");
+        System.out.println("===============================\n");
+        List<String> trainConsist = new ArrayList<>();
 
-        System.out.println("Original Bogie List:");
-        bogies.forEach(System.out::println);
+        System.out.println("Train Initialized successfully");
+        System.out.println("Intial Bogie Count: "+trainConsist.size());
+        System.out.println("Current Train Consist: []");
+        System.out.println("System ready for operations...");
 
-        // Stream filtering: capacity > 60
-        List<PassengerBogie> filteredBogies = bogies.stream()
-                .filter(b -> b.getCapacity() > 60)
-                .collect(Collectors.toList());
-
-        System.out.println("\nFiltered Bogies (capacity > 60):");
-        filteredBogies.forEach(System.out::println);
-
-        // Program continues...
-        System.out.println("\nProgram continues with other operations...");
     }
 }
